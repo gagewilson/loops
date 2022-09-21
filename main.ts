f36330fdd231401a74ccbb242c596af7f183c463
gagewilson <113040604+gagewilson@users.noreply.github.com>
2022-09-21 10:48:06 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 10; index++) {
+        basic.showLeds(`
+            # . # . #
+            # . # . #
+            # # # . #
+            # . # . #
+            # . # . #
+            `)
+        basic.clearScreen()
+        basic.pause(250)
+    }
+})
